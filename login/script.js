6febd761bb0d2775c28b03d3108f13d9d207df1f
Abiel252005 <abielon25@gmail.com>
2025-06-28 12:23:53 -0600

    Primer commit con todo el proyecto EduTech

--- login/script.js
+++ login/script.js
@@ -17,3 +17,25 @@ signInForm.addEventListener("submit", (e) => {
     // Redirect to panel.html upon successful sign-in
     window.location.href = "panel.html";
 });
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyChssYWkaV_YlH_ZPW4bdvNKlDS5TaHPZU",
+    authDomain: "edutech-ad4f9.firebaseapp.com",
+    projectId: "edutech-ad4f9",
+    storageBucket: "edutech-ad4f9.firebasestorage.app",
+    messagingSenderId: "493756404164",
+    appId: "1:493756404164:web:28a89511bd0dfa2ca45491",
+    measurementId: "G-YDKN6WY7CQ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
